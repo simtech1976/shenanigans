c4c76b8e760787133baf3aeba62239044f0ffe38
Simon Paul Page <92449717+simtech1976@users.noreply.github.com>
2026-09-24 16:34:21 +0000

    2408251723

--- db.py
+++ db.py
@@ -5,4 +5,4 @@ from supabase import create_client, Client
 def get_supabase() -> Client:
     return create_client(
         st.secrets['SUPABASE_URL'],
-        st.secrets['SUPABASE_key'])
+        st.secrets['SUPABASE_KEY'])
